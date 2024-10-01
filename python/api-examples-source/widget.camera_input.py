f2b40af6207a81b77894df8f6aa134ff1429215f
Karol Kostrzewa <karol.kostrzewa@snowflake.com>
2024-10-01 11:34:20 +0200

    Update widget.camera_input.py

--- python/api-examples-source/widget.camera_input.py
+++ python/api-examples-source/widget.camera_input.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-enable = st.checkbox("Enable camera test 123")
+enable = st.checkbox("Enable camera test 1234")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
 if picture:
